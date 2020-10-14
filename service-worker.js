7410cb10f0c78882d6e6f51cd83a9f4067b194be
CDF GA DEPLOY <deploy_ga@hcal.cf>
2020-10-14 13:53:17 +0000

    Automatic publish

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.055125b977cf5f11beea71e953c774a7.js"
+  "/precache-manifest.4a3b212fc5816520ddf9a5cc0b02a77f.js"
 );
 
 self.addEventListener('message', (event) => {
